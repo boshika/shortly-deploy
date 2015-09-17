82f4d88b6ae04f8343dc4ee2b5e8aa4c3a991f36
Boshika Tara <boshikatara@gmail.com>
2015-09-17 16:28:31 -0700

    added buildpack

--- app/config.js
+++ app/config.js
@@ -33,8 +33,7 @@ db.knex.schema.hasTable('users').then(function(exists) {
   if (!exists) {
     db.knex.schema.createTable('users', function (user) {
       user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
+      
       user.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
